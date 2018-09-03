661c81109e6adf5d550712f280aea2b5d2830a36
sashaDrobot <alex.drobot22@gmail.com>
2018-09-04 01:59:33 +0300

    v 0.0.2

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+const Dashboard = Vue.component('dashboard', {
+    template: '#dashboard',
+});
+
+const ViewUser = Vue.component('view-profile', {
+    template: '#view-profile',
+});
+
+const router = new VueRouter({
+    // mode: 'history', !#
+    routes: [
+        { path: '/dashboard', component: Dashboard },
+        { path: '/view-profile', component: ViewUser },
+
+        { path: '*', component: Dashboard },
+    ],
+    linkActiveClass: "active"
+});
+
+const vm = new Vue({
+    el: '#app',
+    router: router,
+});
+
 $('#toggler').on('click', function () {
     $('.wrapper').toggleClass('open-sidebar');
+    $(this).toggleClass('collapsed');
+    $('.sidebar .nav .nav-link, .sidebar .nav .profile').click(function () {
+        $('.wrapper').removeClass('open-sidebar');
+        $('#toggler').addClass('collapsed');
+    });
 });
+
